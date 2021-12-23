7cf60cc8c3cdbc749ba73ebdb6fa1cdf35f8b0a8
Leopold Lemmermann <leopold@lemmermann.com>
2021-12-23 10:00:59 +0100

    Initial Commit

---
+++ Acessibility/AcessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcessibilityApp.swift
+//  Acessibility
+//
+//  Created by Leopold Lemmermann on 23.12.21.
+//
+
+import SwiftUI
+
+@main
+struct AcessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Acessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Acessibility
+//
+//  Created by Leopold Lemmermann on 23.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
