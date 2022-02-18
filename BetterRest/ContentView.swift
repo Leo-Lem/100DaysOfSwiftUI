3ec26f2cdeafffaa5e5366ee12a518b96e1b92c8
Leopold Lemmermann <leopold@lemmermann.com>
2022-02-18 13:31:28 +0100

    - Updated for iOS 15. - Added an app icon. - Cleaned up the UI.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -6,102 +6,63 @@
 //
 
 import SwiftUI
+import CoreML
+import MyDates
+import MySwiftUI
 
 struct ContentView: View {
-    
-    static var defaultWakeTime: Date {
-        var comps = DateComponents()
-        comps.hour = 5
-        comps.minute = 55
-        return Calendar.current.date(from: comps) ?? Date()
+    var body: some View {
+        Form {
+            Section("When do you want to wake up?") {
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                    .datePickerStyle(.wheel)
+            }
+            
+            Section("Desired amount of sleep") {
+                Stepper("\(sleep.formatted()) hours", value: $sleep, in: 4...12, step: 0.25)
+            }
+            
+            Section("Daily coffee intake (\(coffee.formatted()) cups)") {
+                Slider(value: $coffee, in: 0...20, step: 1)
+            }
+        }
+        .alert(error?.title ?? "", item: $error, message: { Text($0.message) })
+        .navigationTitle("Bedtime: \(getBedtime() ?? "<Error>")")
+        .embedInNavigation()
+        .navigationViewStyle(.stack)
     }
     
     @State private var wakeUp = defaultWakeTime
-    @State private var sleepAmount = 8.5
-    @State private var coffeeAmount = 0.0
+    @State private var sleep = 8.5
+    @State private var coffee = 0.0
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
-    var body: some View {
-        NavigationView {
-            Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity)
-                        .padding(.bottom)
-                    
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                        .datePickerStyle(WheelDatePickerStyle())
-                }
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity)
-                        .padding(.bottom)
-                    
-                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                        Text("\(sleepAmount, specifier: "%g") hours")
-                    }
-                }
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity)
-                        .padding(.bottom)
-                    
-                    Slider(value: $coffeeAmount, in: 0...20, step: 1)
-                    Text("\(coffeeAmount, specifier: "%g") cup(s)").frame(maxWidth: .infinity)
-                    
-                    /*Picker(selection: $coffeeAmount, label: Text("Daily coffee intake")) {
-                        ForEach(0..<21) { cups in
-                            Text("\(cups) cups")
-                        }
-                    }
-                    
-                    Stepper(value: $coffeeAmount, in: 0...20) {
-                        if coffeeAmount == 1 {
-                            Text("1 Cup")
-                        } else {
-                            Text("\(coffeeAmount) cups")
-                        }
-                    }*/
-                }
-            }
-            .navigationBarTitle("Bedtime: \(estimatedBedtime)")
+    @State private var error: (title: String, message: String)? = nil
+}
+
+extension ContentView {
+    private static let defaultWakeTime: Date = Calendar.current.date(bySettingHour: 6, minute: 55, second: 0, of: Date.now) ?? Date.now
+
+    private func getBedtime() -> String? {
+        do {
+            let bedtime = try estimateBedtime(wakeUp: wakeUp, sleep: sleep, coffee: coffee)
+            return bedtime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            self.error = ("Error", "Sorry, there was a problem calculating your bedtime.")
+            return nil
         }
     }
-
-    var estimatedBedtime: String {
-        let model = SleepCalculator()
-        
-        let estimatedSleep = sleepAmount
-        let coffee = Double(coffeeAmount)
-        let comps = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
-        let hour = (comps.hour ?? 0) * 60^2
-        let minute = (comps.minute ?? 0) * 60
-        let wake = Double(hour + minute)
-        
-        do {
-            let pred = try model.prediction(wake: wake, estimatedSleep: estimatedSleep, coffee: coffee)
-            
-            let sleepTime = wakeUp - pred.actualSleep
-            let formatter = DateFormatter()
-            formatter.timeStyle = .short
-            
-            return formatter.string(from: sleepTime)
-        } catch {
-            print("Error in prediction")
-        }
-        return "Error..."
+    
+    private func estimateBedtime(wakeUp: Date, sleep estimatedSleep: Double, coffee: Double) throws -> Date {
+        let wake = wakeUp.distance(to: wakeUp.startOf(.day) ?? wakeUp, unit: .second)!,
+            config = MLModelConfiguration(),
+            model = try SleepCalculator(configuration: config),
+            pred = try model.prediction(wake: wake, estimatedSleep: estimatedSleep, coffee: coffee)
+        return wakeUp - pred.actualSleep
     }
 }
 
+//MARK: - Previews
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
